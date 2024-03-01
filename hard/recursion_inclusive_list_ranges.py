7fe9839cceb864fe6a297e26779796d7c1bd9ef1
darko <ki14.skakun@gmail.com>
2024-03-01 15:08:37 +0200

    Recursion: Inclusive List Ranges

---
+++ hard/recursion_inclusive_list_ranges.py
@@ -0,0 +1,37 @@
+"""
+Recursion: Inclusive List Ranges
+
+Write a function that, given the start start_num and end end_num values,
+return a list containing all the numbers inclusive to that range. See examples below.
+
+Examples
+inclusive_list(1, 5) ➞ [1, 2, 3, 4, 5]
+
+inclusive_list(2, 8) ➞ [2, 3, 4, 5, 6, 7, 8]
+
+inclusive_list(10, 20) ➞ [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+inclusive_list(17, 5) ➞ [17]
+
+Notes
+The numbers in the list are sorted in ascending order.
+If start_num is greater than end_num, return a list with the higher value. See example #4.
+
+
+"""
+
+
+def inclusive_list(start_num, end_num):
+    if start_num == end_num:
+        return [end_num]
+
+    current = start_num
+    if start_num > end_num:
+        return [start_num]
+
+    start_num += 1
+    return [current] + inclusive_list(start_num, end_num)
+
+
+print(inclusive_list(2,8))
+print(inclusive_list(17, 5))
